56a78b0058d99887359929dec04b1c5537ed8895
Federico Peralta <federicoperaltaquinteros@gmail.com>
2025-05-08 15:15:39 -0300

    Clase 2: Add CRUD to user

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createUser, getUsers } from "../../controllers/users/index.js";
+import {
+  createUser,
+  deleteUser,
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../../controllers/users/index.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/", createUser);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
